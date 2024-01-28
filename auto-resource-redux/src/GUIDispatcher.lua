ae9f7dcde64529283d443dd0ec919eba9844acf4
udf <tabhooked@gmail.com>
2024-01-28 22:28:10 +0200

    dispatcher: add option to always run a handler

--- auto-resource-redux/src/GUIDispatcher.lua
+++ auto-resource-redux/src/GUIDispatcher.lua
@@ -1,27 +1,36 @@
 local GUIDispatcher = {}
 
 -- { on_click = { event_tag = fn, ... }, ... }
+local registered_tagged_events = {
+  [defines.events.on_gui_click] = {},
+  [defines.events.on_gui_closed] = {},
+}
+-- { on_click = { fn1, fn2, ... }, ... }
 local registered_events = {
   [defines.events.on_gui_click] = {},
   [defines.events.on_gui_closed] = {},
 }
 
-function GUIDispatcher.register(event_name, event_tag, handler)
-  registered_events[event_name][event_tag] = handler
+function GUIDispatcher.register(event_name, event_tag, handler, always_run)
+  registered_tagged_events[event_name][event_tag] = handler
+  if always_run then
+    table.insert(registered_events[event_name], handler)
+  end
 end
 
 function GUIDispatcher.on_event(event)
-  if not event.element then
-    return
-  end
-  local tags = event.element.tags
-  local event_tag = tags['event']
-  if not event_tag then
-    return
-  end
-  local handler = registered_events[event.name][event_tag]
+  local player = game.get_player(event.player_index)
+  local tags = (event.element or {}).tags or {}
+  local handler = registered_tagged_events[event.name][tags['event']]
   if handler then
-    handler(event, tags, game.get_player(event.player_index))
+    handler(event, tags, player)
+  end
+
+  -- fire all handlers that accept all events
+  for event_tag, fn in pairs(registered_events[event.name]) do
+    if fn ~= handler then
+      fn(event, nil, player)
+    end
   end
 end
 
