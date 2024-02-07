7d11b2c343fa15a2fce83ea17c4445e1e0dd8d5d
udf <tabhooked@gmail.com>
2024-02-07 21:36:55 +0200

    add sink logistic storage chest
    it doesnt do much yet

--- auto-resource-redux/data.lua
+++ auto-resource-redux/data.lua
@@ -162,7 +162,7 @@ local sink_tank_item = {
   icon = "__auto-resource-redux__/graphics/sink-tank-icon.png",
   icon_size = 64,
   name = "arr-sink-tank",
-  order = "b[fluid]-z[storage-tank]",
+  order = "b[fluid]-z[arr-sink-tank]",
   place_result = "arr-sink-tank",
   stack_size = 50,
   subgroup = "storage",
@@ -173,6 +173,80 @@ data:extend({
   sink_tank,
   sink_tank_recipe,
   sink_tank_item,
+})
+
+
+-- sink chest for bots to insert items into storage
+local sink_chest = table.deepcopy(data.raw["logistic-container"]["logistic-chest-storage"])
+sink_chest.collision_mask = {"item-layer", "object-layer", "water-tile"}
+sink_chest.name = "arr-logistic-sink-chest"
+sink_chest.minable.result = "arr-logistic-sink-chest"
+sink_chest.animation = {
+  layers = {
+    {
+      filename = "__auto-resource-redux__/graphics/sink-chest.png",
+      frame_count = 7,
+      width = 66,
+      height = 32,
+      priority = "extra-high",
+      scale = 0.5,
+      shift = { 0, 0 },
+   },
+    {
+      draw_as_shadow = true,
+      filename = "__auto-resource-redux__/graphics/sink-chest-shadow.png",
+      width = 66,
+      height = 34,
+      priority = "extra-high",
+      repeat_count = 7,
+      scale = 0.5,
+      shift = { 0.078, 0 },
+    }
+  }
+}
+sink_chest.corpse = "arr-logistic-sink-chest-remnants"
+
+local sink_chest_remnants = table.deepcopy(data.raw["corpse"]["storage-chest-remnants"])
+sink_chest_remnants.name = "arr-logistic-sink-chest-remnants"
+sink_chest_remnants.animation = {
+  direction_count = 1,
+  filename = "__auto-resource-redux__/graphics/sink-chest-remnants.png",
+  frame_count = 1,
+  line_length = 1,
+  width = 72,
+  height = 38,
+  scale = 0.5,
+  shift = { 0, -0.03125 },
+}
+
+local sink_chest_recipe = {
+  enabled = true,
+  ingredients = {
+    { type = "item", name = "steel-chest", amount = 1 },
+    { type = "item", name = "electronic-circuit", amount = 3 },
+    { type = "item", name = "advanced-circuit", amount = 1 }
+  },
+  name = "arr-logistic-sink-chest",
+  result = "arr-logistic-sink-chest",
+  type = "recipe"
+}
+
+local sink_chest_item = {
+  icon = "__auto-resource-redux__/graphics/sink-chest-icon.png",
+  icon_size = 64,
+  name = "arr-logistic-sink-chest",
+  order = "b[storage]-f[arr-logistic-sink-chest]",
+  place_result = "arr-logistic-sink-chest",
+  stack_size = 50,
+  subgroup = "logistic-network",
+  type = "item"
+}
+
+data:extend({
+  sink_chest,
+  sink_chest_remnants,
+  sink_chest_recipe,
+  sink_chest_item,
 })
 
 
