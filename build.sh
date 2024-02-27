fcd023b547d12a7172b0da4a505a16e030ccee19
udf <tabhooked@gmail.com>
2024-02-27 21:21:10 +0200

    add build script

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+SRCDIR=auto-resource-redux
+FILENAME="$(jq -r '.name + "_" + .version' $SRCDIR/info.json).zip"
+
+if [[ $(git status --porcelain=v1 "$SRCDIR" 2>/dev/null) ]]; then
+  echo "$SRCDIR has unstaged changes. Exiting."
+  exit 1
+fi
+
+if [ -f "$FILENAME" ]; then
+  read -p "Overwrite $FILENAME? " -n 1 -r
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    rm "$FILENAME"
+  else
+    exit 1
+  fi
+fi
+zip -r "$FILENAME" -r "$SRCDIR"
